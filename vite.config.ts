27323ee63236590921f8a69d78c6ad231be386e1
Hieu <ledthh@gmail.com>
2024-06-05 21:21:07 +0700

    fix: production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: {
       src: "/src",
     },
-    mainFields: ['index'],
+    mainFields: ['index', 'browser'],
   },
 })
